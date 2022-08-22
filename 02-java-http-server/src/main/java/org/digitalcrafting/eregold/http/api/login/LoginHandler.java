73942be20be063ae13d2a42b540175aad614d934
DigitalCrafting <k.kasprzyk2@gmail.com>
2022-08-22 18:54:04 +0200

    02 - Implemented login without session

--- 02-java-http-server/src/main/java/org/digitalcrafting/eregold/http/api/login/LoginHandler.java
+++ 02-java-http-server/src/main/java/org/digitalcrafting/eregold/http/api/login/LoginHandler.java
@@ -3,12 +3,24 @@ package org.digitalcrafting.eregold.http.api.login;
 import com.sun.net.httpserver.HttpExchange;
 import lombok.extern.slf4j.Slf4j;
 import org.digitalcrafting.eregold.http.core.DCAbstractHandler;
+import org.digitalcrafting.eregold.http.core.security.BCrypt;
+import org.digitalcrafting.eregold.http.core.security.JWTUtils;
+import org.digitalcrafting.eregold.http.repository.users.UserEntity;
+import org.digitalcrafting.eregold.http.repository.users.UsersEntityManager;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.CharBuffer;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 public class LoginHandler extends DCAbstractHandler {
+    private UsersEntityManager usersEntityManager;
+
+    public LoginHandler() {
+        usersEntityManager = new UsersEntityManager();
+    }
+
     @Override
     public void handleGet(HttpExchange exchange) {
         String resp = "Login working!\n";
@@ -19,8 +31,22 @@ public class LoginHandler extends DCAbstractHandler {
     public void handlePost(HttpExchange exchange) throws IOException {
         InputStreamReader isr = new InputStreamReader(exchange.getRequestBody());
         LoginRequest request = GSON.fromJson(isr, LoginRequest.class);
-        
-        LoginResponse response = new LoginResponse("very_secret_token");
-        sendResponse(exchange, response);
+
+        UserEntity userEntity = usersEntityManager.getByUserId(request.getUserId());
+        if (userEntity == null) {
+            // TODO sendResponse not found
+            return;
+        }
+
+        byte[] passwordb = StandardCharsets.UTF_8.encode(CharBuffer.wrap(request.getPassword())).array();
+        if (BCrypt.checkpw(passwordb, userEntity.getPasswordHash())) {
+            String token = JWTUtils.generateAccessToken(userEntity.getUserId());
+            // TODO session context
+
+            LoginResponse response = new LoginResponse(token);
+            sendResponse(exchange, response);
+        }
+
+        // TODO sendResponse bad request
     }
 }
---
+++ 02-java-http-server/src/main/java/org/digitalcrafting/eregold/http/core/security/JWTUtils.java
@@ -0,0 +1,60 @@
+package org.digitalcrafting.eregold.http.core.security;
+
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Date;
+
+import static java.lang.String.format;
+
+@Slf4j
+public class JWTUtils {
+    private static final String JWT_SECRET = "zdtlD3JK56m6wTTgsNFhqzjqP";
+    private static final String JWT_ISSUER = "digitalcrafting.org";
+
+    public static String generateAccessToken(String userId) {
+        return Jwts.builder()
+                .setSubject(format("%s", userId))
+                .setIssuer(JWT_ISSUER)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 7 * 24 * 60 * 60 * 1000)) // 1 week
+                .signWith(SignatureAlgorithm.HS512, JWT_SECRET)
+                .compact();
+    }
+
+    public static String getUserId(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(JWT_SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public static Date getExpirationDate(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(JWT_SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getExpiration();
+    }
+
+    public static boolean validate(String token) {
+        try {
+            Jwts.parser().setSigningKey(JWT_SECRET).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex) {
+            log.error("Invalid JWT signature - {}", ex.getMessage());
+        } catch (MalformedJwtException ex) {
+            log.error("Invalid JWT token - {}", ex.getMessage());
+        } catch (ExpiredJwtException ex) {
+            log.error("Expired JWT token - {}", ex.getMessage());
+        } catch (UnsupportedJwtException ex) {
+            log.error("Unsupported JWT token - {}", ex.getMessage());
+        } catch (IllegalArgumentException ex) {
+            log.error("JWT claims string is empty - {}", ex.getMessage());
+        }
+        return false;
+    }
+}
